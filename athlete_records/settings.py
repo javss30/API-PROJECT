99e56253ce887fcab8f2815959a1737778257980
Edwin Macasero <macaserojr11@gmail.com>
2026-04-17 00:56:45 +0800

    modifying the settings.py

--- athlete_records/settings.py
+++ athlete_records/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4t=8p__767qd7!1s+jfd%ee2#dkv_*l(00g*suj5h%8mf9%hi@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
